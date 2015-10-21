18ed9559613d14ba4a8a4b3ab064144dddcfa4c1
rugbertl <rugbertl.gaming@gmx.at>
2015-10-21 15:45:28 +0200

    update f1 mesure

--- Evaluate_Recommender.py
+++ Evaluate_Recommender.py
@@ -14,7 +14,7 @@ ARTISTS_FILE = "UAM_artists.txt"    # artist names for UAM
 USERS_FILE = "UAM_users.txt"        # user names for UAM
 
 NF = 5              # number of folds to perform in cross-validation
-
+K = 5
 
 # Function to read metadata (users or artists)
 def read_from_file(filename):
@@ -32,7 +32,8 @@ def read_from_file(filename):
 # Function that implements a CF recommender. It takes as input the UAM, metadata (artists and users),
 # the index of the seed user (to make predictions for) and the indices of the seed user's training artists.
 # It returns a list of recommended artist indices
-def recommend_CF(UAM, seed_uidx, seed_aidx_train):
+def recommend_CF(UAM, seed_uidx, seed_aidx_train, K = 1):
+
     # UAM               user-artist-matrix
     # seed_uidx         user index of seed user
     # seed_aidx_train   indices of training artists for seed user
@@ -141,6 +142,7 @@ if __name__ == '__main__':
             # Call recommend function
             copy_UAM = UAM.copy()       # we need to create a copy of the UAM, otherwise modifications within recommend function will effect the variable
             #rec_aidx = recommend_CF(copy_UAM, u, train_aidx)
+            #rec_aidx = recommend_CF(copy_UAM, u, train_aidx, K)
             rec_aidx = recommend_baseline(copy_UAM, u, train_aidx)
             print "Recommended items: ", len(rec_aidx)
 
@@ -160,11 +162,16 @@ if __name__ == '__main__':
             avg_prec += prec / (NF * no_users)
             avg_rec += rec / (NF * no_users)
 
-            # Output precision and recall of current fold
+             # Output precision and recall of current fold
             print ("\tPrecision: %.2f, Recall:  %.2f" % (prec, rec))
 
             # Increase fold counter
             fold += 1
 
+    # calculate f1 measure
+    f1 = 2 * ((avg_prec * avg_rec) / (avg_prec + avg_rec))
+
     # Output mean average precision and recall
-    print ("\nMAP: %.2f, MAR  %.2f" % (avg_prec, avg_rec))
+    print ("\nMAP: %.2f, MAR: %.2f, F1 measure: %.2f" % (avg_prec, avg_rec, f1))
+
+
